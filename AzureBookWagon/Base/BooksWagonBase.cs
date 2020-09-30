05333b38afe0a88ab1a5d493420ef905066f956d
sidthakur258 <sidthakur258@gmail.com>
2020-09-30 12:56:50 +0530

    Add project files.

---
+++ AzureBookWagon/Base/BooksWagonBase.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AzureBookWagon.Base
+{
+    public class BooksWagonBase
+    {
+        public IWebDriver driver;
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized", "--disable-notifications");
+            driver = new ChromeDriver(options);
+            driver.Url = "https://www.bookswagon.com/login";
+        }
+
+        [OneTimeTearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+    }
+}
